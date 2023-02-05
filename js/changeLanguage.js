3cdcec0ad4c8cafaea2d56507ed14ee4ee61d429
Eke <ereklesabbather@gmail.com>
2023-02-05 18:28:58 +0400

    test

--- js/changeLanguage.js
+++ js/changeLanguage.js
@@ -1,13 +1,64 @@
+let currentLang = "en";
+
+if (!hasTouch())
+{
+  lang_btn_en.addEventListener("mouseover", () => {
+    if (currentLang != "en")
+      lang_btn_en_flag.style.transform = "scale(1.1)";
+  })
+  lang_btn_en.addEventListener("mouseout", () => {
+    lang_btn_en_flag.style.transform = "scale(1)";
+  })
+
+  lang_btn_ge.addEventListener("mouseover", () => {
+    if (currentLang != "ge")
+      lang_btn_ge_flag.style.transform = "scale(1.1)";
+  })
+  lang_btn_ge.addEventListener("mouseout", () => {
+    lang_btn_ge_flag.style.transform = "scale(1)";
+  })
+}
+
+lang_btn_en.addEventListener("click", () => 
+{
+  changeLanguageTo("en");
+  lang_btn_en.style.outline = "1px solid var(--brown1)";
+  lang_btn_ge.style.outline = "none";
+  lang_btn_en.style.cursor = "auto"
+  lang_btn_ge.style.cursor = "url('images/Hand_Cyan_Right.svg'), auto"
+  lang_btn_en_flag.style.transform = "scale(1)";
+})
+
+lang_btn_ge.addEventListener("click", () => 
+{
+  changeLanguageTo("ge");
+  lang_btn_ge.style.outline = "1px solid var(--brown1)";
+  lang_btn_en.style.outline = "none";
+  lang_btn_ge.style.cursor = "auto"
+  lang_btn_en.style.cursor = "url('images/Hand_Cyan_Right.svg'), auto"
+  lang_btn_ge_flag.style.transform = "scale(1)";
+})
+
 function changeLanguageTo(lang)
 {
-  if (lang == "ge")
+  currentLang = lang;
+  
+  if (lang == "gez")
   {
-    ge_button.disabled = true;
-    en_button.disabled = false;
-    olala_txt_en.innerHTML = "ლორემ იპსუმ";
-    olala_txt_en.id = "olala_txt_ge";
     and_life_begins_txt_en.innerHTML = "... და სიცოცხლე იწყება ...";
     and_life_begins_txt_en.id = "and_life_begins_txt_ge";
+
+    olala_txt_1.innerHTML = "";
+    olala_txt_2.innerHTML = "";
+    olala_txt_3.innerHTML = "";
+    olala_txt_4.innerHTML = "";
+
+    let olalaTxts = document.getElementsByClassName("olala_txt");
+    for (let i = 0; i < olalaTxts.length; i++)
+    {
+      olalaTxts[i].classList.remove = "olala_txt_en";
+      olalaTxts[i].classList.add = "olala_txt_ge";
+    }
     
     let ourBrands = document.getElementsByClassName("our_brands_txt");
     for (let i = 0; i < ourBrands.length; i++)
@@ -32,17 +83,23 @@ function changeLanguageTo(lang)
       photosBtn[i].classList.remove = "change_photos_btn_en";
       photosBtn[i].classList.add = "change_photos_btn_ge";
     }
-
-    
   }
-  else if (lang == "en")
+  else if (lang == "enz")
   {
-    en_button.disabled = true;
-    ge_button.disabled = false;
-    olala_txt_ge.innerHTML = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam";
-    olala_txt_ge.id = "olala_txt_en";
     and_life_begins_txt_ge.innerHTML = "... and life begins ..."
     and_life_begins_txt_ge.id = "and_life_begins_txt_en";
+
+    olala_txt_1.innerHTML = "Parenthood is the greatest adventure you can ever go on. Here in O’lala Baby, we believe, that  every family deserves the best to bring colorful, creative expression to this journey.";
+    olala_txt_2.innerHTML = "Our passion for providing only the finest and most trusted baby products for you and your little one, drives us to carefully curate our brands and offer you the highest standards of style, quality and safety.";
+    olala_txt_3.innerHTML = "And we don't just stop at offering top-quality products in the universe.";
+    olala_txt_4.innerHTML = "Our commitment to exceptional customer service proudly sets us apart.";
+
+    let olalaTxts = document.getElementsByClassName("olala_txt");
+    for (let i = 0; i < olalaTxts.length; i++)
+    {
+      olalaTxts[i].classList.remove = "olala_txt_en";
+      olalaTxts[i].classList.add = "olala_txt_ge";
+    }
 
     let ourBrands = document.getElementsByClassName("our_brands_txt");
     for (let i = 0; i < ourBrands.length; i++)
--- js/showcase.js
+++ js/showcase.js
@@ -1,35 +1,54 @@
 showcase_1.addEventListener("click", (evt) => changeLargeItem(evt, "showcase_1"))
-  showcase_2.addEventListener("click", (evt) => changeLargeItem(evt, "showcase_2"))
+showcase_2.addEventListener("click", (evt) => changeLargeItem(evt, "showcase_2"))
 
+if (!hasTouch())
+{
   showcase_1.addEventListener("mouseover", (evt) => scaleDown(evt))
   showcase_1.addEventListener("mouseout", (evt) => scaleBack(evt))
   showcase_2.addEventListener("mouseover", (evt) => scaleDown(evt))
   showcase_2.addEventListener("mouseout", (evt) => scaleBack(evt))
+}
 
-  function changeLargeItem(evt, showcaseName)
+
+function changeLargeItem(evt, showcaseName)
+{
+  if (evt.target.classList[0] == "showcase_item")
   {
-    if (evt.target.classList[0] == "showcase_item")
+    let showcaseItems = document.getElementsByClassName("showcase_item");
+    let startIndex = 0;
+
+    if (showcaseName == "showcase_2")
+      startIndex = 30;
+
+    for (let i = startIndex; i < startIndex + 30; i++)
     {
-      let preloadImage = new Image();
-      preloadImage.src = "images/showcase/" + evt.target.alt + "_270.jpg";
-
-      let largeItem = document.getElementById(showcaseName + "_large_item");
-      largeItem.style.transform = "scale(0)";
-      setTimeout(() => 
-      {
-        document.getElementById(showcaseName + "_item_name").innerHTML = evt.target.name;
-        largeItem.src  = "images/showcase/" + evt.target.alt + "_270.jpg";
-        largeItem.style.transform = "scale(1)";
-      }, 1100);
+      showcaseItems[i].style.outline = "none";
+      showcaseItems[i].style.borderRadius = "0";
     }
+
+    evt.target.style.outline = "1px solid var(--brown1)";
+    evt.target.style.borderRadius = "8px";
+
+    let preloadImage = new Image();
+    preloadImage.src = "images/showcase/" + evt.target.alt + "_270.jpg";
+
+    let largeItem = document.getElementById(showcaseName + "_large_item");
+    largeItem.style.transform = "scale(0)";
+    setTimeout(() => 
+    {
+      document.getElementById(showcaseName + "_item_name").innerHTML = evt.target.name;
+      largeItem.src  = "images/showcase/" + evt.target.alt + "_270.jpg";
+      largeItem.style.transform = "scale(1)";
+    }, 1100);
   }
-  function scaleDown(evt) 
-  {
-    if (evt.target.classList[0] == "showcase_item")
-      evt.target.style.transform = "scale(0.9)";
-  }
-  function scaleBack(evt)
-  {
-    if (evt.target.classList[0] == "showcase_item")
-      evt.target.style.transform = "scale(1)";
-  }
+}
+function scaleDown(evt) 
+{
+  if (evt.target.classList[0] == "showcase_item")
+    evt.target.style.transform = "scale(0.9)";
+}
+function scaleBack(evt)
+{
+  if (evt.target.classList[0] == "showcase_item")
+    evt.target.style.transform = "scale(1)";
+}
